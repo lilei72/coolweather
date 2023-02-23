d866f7306728cf7667043f16444679db18119ca4
lilei <1754788580@qq.com>
2023-02-23 16:54:05 +0800

    1.获取天气功能开发 2.显示天气功能开发
    ps：现在无法显示suggest里的comfort，原因可能是接口没有提供这个数据或者是我代码有问题

--- app/src/main/java/com/example/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -1,5 +1,14 @@
 package com.example.coolweather.util;
 
-public class HttpUtil {
+import android.util.Log;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 }
